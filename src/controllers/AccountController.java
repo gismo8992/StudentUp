a75ddea90934026326c19b7f877c738f0debe089
Ivan <gismo8992@mail.ru>
2024-02-25 01:37:31 +0300

    Add comments for method

--- src/controllers/AccountController.java
+++ src/controllers/AccountController.java
@@ -9,6 +9,12 @@ public class AccountController {
     public static  <T extends WorkingPerson, V> void paySalary(T person, V salary) {
         System.out.println(person.getName() + " выплаченная зарплата: " + salary);
     }
+
+    /**
+     * @apiNote Метод для определения среднего возраста людей из списка. Тип человека ограничивается наследием класса Person
+     * @param list лист с объектами-наследниками класса Person для определения среднего возраста
+     * @param <M> тип-наследник класса Person
+     */
     public static <M extends Person> void averageAge(List<M> list) {
         double sumForAverageAge = 0;
         for (M m : list) {
